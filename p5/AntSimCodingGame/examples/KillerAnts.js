8766d1d2635bd14beaae8ca5de5c3ca9f25a5117
Björn Bosse <b.bosse1991@gmail.com>
2020-05-03 08:19:04 +0200

    Added example colony: Killer Ants

---
+++ p5/AntSimCodingGame/examples/KillerAnts.js
@@ -0,0 +1,40 @@
+var PLAYER_INFO = {
+    name: 'Björn Bosse',
+    colonyName: 'Killer Ants',
+    castes: [
+        { name: 'killer', color: 'red', speed: -1, rotationSpeed: -1, load: -1, range: -1, viewRange: 0, vitality: 2, attack: 2 }
+    ]
+};
+
+class PlayerAnt extends BaseAnt {
+    determineCaste(availableInsects) {
+        return 'killer';
+    }
+
+    waits() {
+        this.goForward(40);
+        this.turnByDegrees(random(-10, 10));
+    }
+    
+    spotsBug(bug) {
+        this.setMarker(0, 150);
+        this.attackTarget(bug);
+    }
+
+    smellsFriend(marker) {
+        if (!this.target)
+            this.goToTarget(marker);
+    }
+
+    becomesTired() {
+        this.goHome();
+    }
+
+    tick() {
+        if (this.range - this.traveledDistance - 100 < this.distanceToAntHill)
+            this.goHome();
+        if (this.vitality >= this.maxVitality * 2 / 3)
+            return;
+        this.goHome();
+    }
+}
