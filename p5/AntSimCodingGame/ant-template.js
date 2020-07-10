b1c788867578a03e3c7ad54807a0c44bef6029f5
Björn Bosse <b.bosse1991@gmail.com>
2020-07-10 11:21:37 +0200

    Simplified AntTemplate

--- p5/AntSimCodingGame/ant-template.js
+++ p5/AntSimCodingGame/ant-template.js
@@ -7,7 +7,7 @@ var PLAYER_INFO = {
 };
 
 class PlayerAnt extends BaseAnt {
-    // this function gets called if the ant is created
+    // this function gets called once if the ant is born
     constructor() {
         super(); // needs to be called first
 
@@ -20,65 +20,10 @@ class PlayerAnt extends BaseAnt {
         return 'default';
     }
 
-    // EVENTS
-    // waits() {}
-    // spotsSugar(sugar) {}
-    // spotsFruit(fruit) {}
-    // spotsBug(bug) {}
-    // spotsFriend(ant) {}
-    // smellsFriend(marker) {}
-    // sugarReached(sugar) {}
-    // fruitReached(fruit) {}
-    // becomesTired() {}
-    // underAttack(bug) {}
-    // hasDied(death) {}
-    // tick() {}
-
-    // COMMANDS
-    // this.goForward(distance)
-    // this.goToTarget(target)
-    // this.goAwayFromTarget(target)
-    // this.goHome()
-    // this.stop()
-    // this.turnByDegrees(angle)
-    // this.turnToTarget(target)
-    // this.turnToDirection(direction)
-    // this.turnAround()
-    // this.think(message)
-    // this.take(food)
-    // this.drop()
-    // this.needsCarriers(fruit)
-    // this.setMarker(information, spread)
-    // this.attackTarget(insect)
-
-    // CASTE-RELATED
-    // this.maxVitality
-    // this.maxLoad
-    // this.maxSpeed
-    // this.rotationSpeed
-    // this.range
-    // this.viewRange
-    // this.attack
-
-    // STATE
-    // this.target
-    // this.remainingDistance
-    // this.remainingRotation
-    // this.traveledDistance
-    // this.direction
-    // this.vitality
-    // this.currentSpeed
-    // this.currentLoad
-    // this.carriedFruit
-    // this.caste
-    // this.target
-    // this.isTired
-    // this.distanceToAntHill
-    // this.antsInViewRange
-    // this.antsFromSameCasteInViewRange
-    // this.bugsInViewRange
-
-    // UTLITIES
-    // Coordinate.distance(coordinate1, coordinate2)
-    // Coordinate.directionAngle(coordinate1, coordinate2)
+    // this function is called, if the ant has nothing to do 
+    // and is waiting for commands
+    waits() {
+        // just go forward for now...
+        this.goForward();
+    }
 }
